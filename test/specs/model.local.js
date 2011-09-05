18bc5ba02fbb80055caeefa6d185e7cea9301a96
Brandon Mason <brandonjmason@gmail.com>
2011-09-04 22:24:23 -0700

    made test verify attribute persistance, made Model.setup accept multiple attributes

--- test/specs/model.local.js
+++ test/specs/model.local.js
@@ -2,16 +2,19 @@ describe("Model.Local", function(){
   var User;
   
   beforeEach(function(){
-    User = Spine.Model.setup("User", "name")
+    User = Spine.Model.setup("User", "first", "last")
   });
   
   it("should persist attributes", function(){
     User.extend(Spine.Model.Local);
     
-    User.create({name: "Bob"});
+    User.create({first: "Bob", last: "McCreedy"});
     User.records = {};
     
     User.loadLocal();
-    expect(User.first()).toBeTruthy();
+    bob = User.first();
+    expect(bob).toBeTruthy();
+    expect(bob.first).toEqual("Bob");
+    expect(bob.last).toEqual("McCreedy");
   });
 });
